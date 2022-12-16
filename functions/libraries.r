b4ba5a38b14b3056778d9478da53de1aedd595bb
1055572 <120114647+1055572@users.noreply.github.com>
2022-12-16 03:32:45 +0000

    Add files via upload

---
+++ functions/libraries.r
@@ -0,0 +1,7 @@
+# Function - libraries
+library(palmerpenguins)
+library(ggplot2)
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(tidyr)
+library(svglite)
