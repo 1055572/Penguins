e0eef6691fff0a7f392018b176139a79bd619fda
1055572 <120114647+1055572@users.noreply.github.com>
2022-12-16 04:39:48 +0000

    Update plotting.r

--- functions/plotting.r
+++ functions/plotting.r
@@ -3,10 +3,10 @@ plot_AG_MxCL_fig <- function(AG_MxCL){
   AG_MxCL %>% 
     ggplot( aes(x = body_mass_g, y = culmen_length_mm)) +
     ggtitle("The relationship between body mass and culmen length of penguins on Biscoe Island") +
-    geom_smooth(method = "lm" ) +
+        geom_smooth(method = "lm" ) +
     geom_point(aes(color = species) , alpha = 0.4, show.legend = TRUE) +
     scale_color_manual(values = c("blue","purple")) +
     labs(x = "Body mass (g)", 
-         y = "Culmen length (mm)") +
+        y = "Culmen length (mm)") +
     theme_bw()
 }
